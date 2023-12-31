cd5ac0387dedd1c68dc662bbed306f097797c627
Vipul-Cariappa <vipulcariappa@.com>
2023-12-31 20:23:53 +0530

    fix? | THIS SHOULD BE THE ROLLBACK POINT

--- src/main.c
+++ src/main.c
@@ -13,3 +13,16 @@ const char *filename;
 bool cli_interpretation_mode = false;
 int interactive_interpretation();
 int file_interpretation(const char *file_name, int input);
+
+int init_language() {
+    cli_interpretation_mode = true;
+    ast = ast_table_new(100);
+    globalBooleans = boolean_table_new(100);
+    globalIntegers = integer_table_new(100);
+    return 0;
+}
+
+int execute_string(const char *string) {
+    yy_scan_string(string);
+    return yyparse();
+}
